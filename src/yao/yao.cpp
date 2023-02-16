818e48e9533aa6280e5056258b847da98a135eeb
oxheron <96091489+0xheron@users.noreply.github.com>
2023-02-16 00:18:05 +0000

    yao

--- src/yao/yao.cpp
+++ src/yao/yao.cpp
@@ -1,48 +1,11 @@
 #include "yao/yao.h"
 
-std::array<std::array<uint8_t, 256>, 16> gen_subst_tables(uint8_t* keyptr)
+YaoCipher::YaoCipher()
 {
-    std::array<std::array<uint8_t, 256>, 16> rval;
+    keyptr = gen_key();
 
-    for (size_t i = 0; i < 16; i++)
-    {
-        // Xor keyptr by hash
-        // Hash that again 
-        rval[i] = gen_rnd_array<256>(keyptr);
-
-        auto hash = XXH3_128bits(keyptr, 16);
-        *((uint64_t*) keyptr) ^= hash.low64;
-        *(((uint64_t*) keyptr) + 1) ^= hash.high64;
-    }
-
-    return rval;
-} 
-
-std::pair<std::array<std::array<uint8_t, 64>, ROUND_NUM / 3>, std::array<std::array<uint8_t, 36>, ROUND_NUM / 3>> gen_transpos_tables(uint8_t* keyptr)
-{   
-    size_t old_kp_low = *((uint64_t*) keyptr);
-    *((uint64_t*) keyptr) = ~*(((uint64_t*) keyptr) + 1);
-    *(((uint64_t*) keyptr) + 1) = ~old_kp_low;
-
-    std::array<std::array<uint8_t, 64>, ROUND_NUM / 3> transpose_8x8;
-    std::array<std::array<uint8_t, 36>, ROUND_NUM / 3> transpose_6x6;
-
-    for (size_t i = 0; i < ROUND_NUM / 3; i++)
-    {
-        // Xor keyptr by hash
-        // Hash that again 
-        // Gen a random array for transposing 8x8's
-        transpose_8x8[i] = gen_rnd_array<64>(keyptr);
-        auto hash = XXH3_128bits(keyptr, 16);
-        *((uint64_t*) keyptr) ^= hash.low64;
-        *(((uint64_t*) keyptr) + 1) ^= hash.high64;
-
-        // Gen a random array for transposing 6x6's
-        transpose_6x6[i] = gen_rnd_array<36>(keyptr);
-        hash = XXH3_128bits(keyptr, 16);
-        *((uint64_t*) keyptr) ^= hash.low64;
-        *(((uint64_t*) keyptr) + 1) ^= hash.high64;
-    }
-
-    return {transpose_8x8, transpose_6x6};
+    subst_tables = gen_tables<256, 6>;
+    transpos_tables = gen_tables<16, 6>;
 }
+
+
--- src/yao/yao.h
+++ src/yao/yao.h
@@ -1,25 +1,22 @@
 #pragma once
 
-// The number of rounds the cipher has 
-// Must be multiple of 3
-// 1 round is either a subsitute / mix random 
-// Or transpose and convolute
-// So order will be subs, mix, trans, conv, trans, conv, and that repeats (that is 3 'rounds')
-#define ROUND_NUM 12
-
 // XXhash library
-// Hopefully we will use our own hash at some point
+// Hopefully we will use our own hash at some point (forrus++)
 #include "xxhash.h"
+
+// Matrix 
+#include "Eigen/Core"
+#include "Eigen/Dense"
 
 // std
 #include <array>
 
-// Return an array of size ct with elements from 0 to ct - 1
-template <int ct> 
-constexpr std::array<uint8_t, ct> gen_init_array()
+// Return an array of size C with elements from 0 to C - 1
+template <int C> 
+constexpr std::array<uint8_t, C> gen_init_array()
 {
-    std::array<uint8_t, ct> rt_array;
-    for (size_t i = 0; i < ct; i++)
+    std::array<uint8_t, C> rt_array;
+    for (size_t i = 0; i < C; i++)
     {
         rt_array[i] = i;
     }
@@ -27,17 +24,17 @@ constexpr std::array<uint8_t, ct> gen_init_array()
     return rt_array;
 }
 
-// Return a random array of size ct, with 1 random element of each from 0 to ct - 1
+// Return a random array of size C, with 1 random element of each from 0 to C - 1
 // This will be in a random order 
 // Uses Knuth shuffling algorithm
-template <int ct> 
-std::array<uint8_t, ct> gen_rnd_array(uint8_t* keyptr)
+template <int C> 
+std::array<uint8_t, C> gen_rnd_array(uint8_t* keyptr)
 {
     auto hash = XXH3_128bits(keyptr, 16);
     size_t xor_hash = hash.low64 ^ hash.high64;
-    std::array<uint8_t, ct> swapped_array = gen_init_array<ct>();
+    std::array<uint8_t, C> swapped_array = gen_init_array<C>();
 
-    for (int i = ct - 1; i > 0; i--)
+    for (int i = C - 1; i > 0; i--)
     {
         std::swap(swapped_array[xor_hash % i], swapped_array[i]);
         hash = XXH3_128bits(&hash, 16);
@@ -47,9 +44,62 @@ std::array<uint8_t, ct> gen_rnd_array(uint8_t* keyptr)
     return swapped_array;
 }
 
-// Generate the substitution tables
-// Pass in the key as a uint8_t ptr for now
-std::array<std::array<uint8_t, 256>, 16> gen_subst_tables(uint8_t* key);
+// Used to generate tables
+// T is the size of each table, R is the number of tables
+template <int T, int R> 
+std::array<std::array<uint8_t, T>, R> gen_tables(uint8_t* keyptr)
+{
+    std::array<std::array<uint8_t, T>, R> rval;
 
-// Generate the transposition tables
-std::pair<std::array<std::array<uint8_t, 64>, ROUND_NUM / 3>, std::array<std::array<uint8_t, 36>, ROUND_NUM / 3>> gen_transpos_tables(uint8_t* kptr);
+    for (size_t i = 0; i < 6; i++)
+    {
+        // Xor keyptr by hash
+        // Hash that again 
+        rval[i] = gen_rnd_array<T>(keyptr);
+
+        auto hash = XXH3_128bits(keyptr, 16);
+        *((uint64_t*) keyptr) ^= hash.low64;
+        *(((uint64_t*) keyptr) + 1) ^= hash.high64;
+    }
+
+    return rval;
+} 
+
+uint8_t* gen_key()
+{   
+    return new uint8_t[16];
+}
+
+// The main class for 
+class YaoCipher
+{
+private:
+    // The key (a pointer, its 128 bits)
+    uint8_t* keyptr = nullptr;
+
+    // Tables for substitution and tranposition
+    std::array<std::array<uint8_t, 256>, 6> subst_tables;
+    std::array<std::array<uint8_t, 16>, 6> transpos_tables;
+
+    // The random shift data for the numbers
+
+public: 
+    YaoCipher(); 
+    ~YaoCipher() { delete[] keyptr; }
+
+    // Encrypt a block
+    Eigen::Matrix<uint32_t, 4, 4> encrypt(const Eigen::Matrix<uint32_t, 4, 4>& input);
+    // Encrypt a string
+    std::string encrypt(const std::string& input);
+
+    // Decrypt a block
+    Eigen::Matrix<uint32_t, 4, 4> decrypt(const Eigen::Matrix<uint32_t, 4, 4>& input);
+    // Decrypt a string
+    std::string decrypt(const std::string& input);
+
+private:
+    // Does one round
+    Eigen::Matrix<uint32_t, 4, 4> round(const Eigen::Matrix<uint32_t, 4, 4>& input);
+    // Undoes one round
+    Eigne::Matrix<uint32_t, 4, 4> unround(const Eigen::Matrix<uint32_t, 4, 4>& input);
+};
