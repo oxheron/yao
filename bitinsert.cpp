b8cc4744907280a97e8f93c660218ebdff282a3e
Echo <96091489+EchoDevelopment@users.noreply.github.com>
2023-02-20 16:07:03 -0800

    Cleanup

--- bitinsert.cpp
+++
@@ -1,7 +0,0 @@
-using uint32_t = unsigned int;
-using size_t = unsigned long long;
-
-uint32_t remove_bit(uint32_t n, size_t position)
-{
-    return (~((uint32_t) (1 << (position)) - 1) & (n >> 1)) | (n & ((1 << (position)) - 1));
-}
